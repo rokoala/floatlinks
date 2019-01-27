e5a71684fb5bdc7ec3f2fd2bd1f780c53031be68
RoKoga <koga.rod@gmail.com>
2019-01-27 10:31:17 -0200

    Add conn

---
+++ server/conn/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const {
+  server,
+  database,
+  user,
+  password
+} = require('../config/database.config.json');
+
+const connectionURI =
+  process.env.MONGODB_URI ||
+  `mongodb://${user}:${password}@${server}/${database}`;
+
+mongoose.connect(
+  connectionURI,
+  { useCreateIndex: true, useNewUrlParser: true }
+);
+
+module.exports = mongoose;
--- server/models/customer.model.js
+++ server/models/customer.model.js
@@ -1,22 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require('../conn');
 
-const server = 'localhost:27017';
-const database = 'floatlinks';
-const user = 'floatlinks';
-const password = 'floatlinks';
-
-mongoose.connect(
-  `mongodb://${user}:${password}@${server}/${database}`,
-  { useCreateIndex: true, useNewUrlParser: true }
+module.exports = mongoose.model(
+  'Customer',
+  new mongoose.Schema({
+    name: String,
+    phone: {
+      type: Number,
+      required: true,
+      unique: true
+    }
+  })
 );
-
-const CustomerSchema = new mongoose.Schema({
-  name: String,
-  phone: {
-    type: Number,
-    required: true,
-    unique: true
-  }
-});
-
-module.exports = mongoose.model('Customer', CustomerSchema);
