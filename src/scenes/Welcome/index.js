6357cb46bab33f9c46145497c4981d58cd176aa3
marcelo.mansur <mansur.ufmg@gmail.com>
2019-01-29 20:24:45 -0200

    Add schedule list at welcome scene

--- src/scenes/Welcome/index.js
+++ src/scenes/Welcome/index.js
@@ -1,12 +1,41 @@
 import React, { PureComponent } from 'react';
-import { Typography, Card, Button } from '@material-ui/core';
+import { Typography, Card, Button, List, ListItem, ListItemText, ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import DeleteRounded from '@material-ui/icons/DeleteRounded';
 import AlarmIcon from '@material-ui/icons/Alarm';
 import { Link } from 'react-router-dom';
 import Layout from '../Layout';
 import './Welcome.css';
 
 export default class Welcome extends PureComponent {
+  constructor (props) {
+    super(props);
+    this.state = {
+      schedule: [{}]
+    }
+  };
+  componentDidMount() {
+    // chamada API REST: getSchedule
+    this.schedule = [{nome:'agendamento1'}, {nome:'agendamento2'}];
+    this.setState({
+      schedule: this.schedule
+    });
+  }
+
   render() {
+    const scheduleList = this.state.schedule.map(x => (
+      <List dense='false'>
+          <ListItem>
+            <ListItemText
+              primary={x.nome}
+            />
+            <ListItemSecondaryAction>
+              <IconButton aria-label="Delete">
+                <DeleteRounded />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+      </List> 
+    ));
     return (
       <Layout {...this.props}>
         <Button
@@ -21,7 +50,7 @@ export default class Welcome extends PureComponent {
           Agendar
         </Button>
         <Card style={{ margin: 15, padding: 25 }}>
-          <Typography variant="subtitle1">Sem agendamentos</Typography>
+          <Typography variant="subtitle1">{scheduleList}</Typography>
         </Card>
       </Layout>
     );
