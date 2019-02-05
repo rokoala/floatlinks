80a7bc9019beaeec1b393830fd166f861b202fac
RoKoga <koga.rod@gmail.com>
2019-02-05 16:08:12 -0200

    Fix Bug of response data after update name on login

--- src/reducers/customer.js
+++ src/reducers/customer.js
@@ -1,6 +1,8 @@
 import { customerActions } from '../actions/actionTypes';
 
-const initialState = {};
+const initialState = {
+  name: ''
+};
 
 export const customer = (state = initialState, action) => {
   switch (action.type) {
