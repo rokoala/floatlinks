10adb3308e0d39a0ffa4c2447ecd8187c95e6720
rokoga <rokoga@gmail.com>
2019-02-05 11:13:33 -0200

    Changes on footer

--- src/scenes/Admin/Layout.js
+++ src/scenes/Admin/Layout.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import { Button, Typography, IconButton } from '@material-ui/core';
+import {
+  Button,
+  BottomNavigation,
+  BottomNavigationAction,
+  Typography,
+  IconButton
+} from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import CalendarIcon from '@material-ui/icons/CalendarToday';
 import LoadingOverlay from 'react-loading-overlay';
@@ -20,95 +26,94 @@ const StyledButton = withStyles({
   }
 })(Button);
 
-const Layout = props => {
-  const { serviceProvider } = props;
-  const IconButtonSignout = withRouter(({ history }) => (
-    <IconButton
-      onClick={() => {
-        // Handle logout with promise... remove session storage/cookie
-        // this.props.logout();
-        history.push('/adminLogin');
-      }}
-    >
-      <ExitIcon />
-    </IconButton>
-  ));
-
-  const ServiceProviderButton = withRouter(({ history }) => (
-    <Button
-      onClick={event => {
-        history.push('/admin');
-      }}
-      style={{ textTransform: 'none' }}
-    >
-      <Typography style={{ textTransform: 'capitalize' }} variant="h6">
-        {serviceProvider.name}
-      </Typography>
-    </Button>
-  ));
-
-  const LayoutWithRouter = withRouter(({ history }) => (
-    <div className="layout">
-      <LoadingOverlay
-        active={false}
-        spinner={<PropagateLoader color={'#36D7B7'} />}
+class Layout extends PureComponent {
+  state = {
+    value: 'agenda'
+  };
+  render() {
+    const { serviceProvider } = this.props;
+    const IconButtonSignout = withRouter(({ history }) => (
+      <IconButton
+        onClick={() => {
+          // Handle logout with promise... remove session storage/cookie
+          // this.props.logout();
+          history.push('/adminLogin');
+        }}
       >
-        <header>
-          <ServiceProviderButton />
-          <div className="user-wrapper">
-            <IconButtonSignout />
-          </div>
-        </header>
+        <ExitIcon />
+      </IconButton>
+    ));
 
-        <div className="flex flex-column align-items-center content">
-          {props.children}
-        </div>
-        <footer
-          style={{
-            position: 'fixed',
-            bottom: 0,
-            width: '100%',
-            backgroundColor: 'white',
-            display: 'flex',
-            justifyContent: 'space-around',
-            boxShadow: '0px 0px 3px lightgrey',
-            borderTop: '1px solid rgba(0,0,0,0.09)'
-          }}
+    const ServiceProviderButton = withRouter(({ history }) => (
+      <Button
+        onClick={event => {
+          history.push('/admin');
+        }}
+        style={{ textTransform: 'none' }}
+      >
+        <Typography style={{ textTransform: 'capitalize' }} variant="h6">
+          {serviceProvider.name}
+        </Typography>
+      </Button>
+    ));
+
+    const LayoutWithRouter = withRouter(({ history }) => (
+      <div className="layout">
+        <LoadingOverlay
+          active={false}
+          spinner={<PropagateLoader color={'#36D7B7'} />}
         >
-          <StyledButton
-            onClick={event => {
-              history.push('/admin');
-            }}
-            style={{ display: 'flex', flexDirection: 'column' }}
-          >
-            <CalendarIcon style={{ color: '#5353bf' }} />
-            <Typography variant="caption">Agenda</Typography>
-          </StyledButton>
-          <StyledButton
-            onClick={event => {
-              history.push('/admin/newCustomer');
-            }}
-            style={{ display: 'flex', flexDirection: 'column' }}
-          >
-            <PersonAddIcon style={{ color: '#52af52' }} />
-            <Typography variant="caption">Novo Cliente</Typography>
-          </StyledButton>
-          <StyledButton
-            onClick={event => {
-              history.push('/admin/nextAppointments');
-            }}
-            style={{ display: 'flex', flexDirection: 'column' }}
-          >
-            <ScheduleIcon style={{ color: '#e06e6e' }} />
-            <Typography variant="caption">Retornos</Typography>
-          </StyledButton>
-        </footer>
-      </LoadingOverlay>
-    </div>
-  ));
+          <header>
+            <ServiceProviderButton />
+            <div className="user-wrapper">
+              <IconButtonSignout />
+            </div>
+          </header>
 
-  return <LayoutWithRouter />;
-};
+          <div className="flex flex-column align-items-center content">
+            {this.props.children}
+          </div>
+          <BottomNavigation
+            style={{
+              position: 'fixed',
+              bottom: 0,
+              width: '100%',
+              display: 'flex',
+              justifyContent: 'space-around',
+              boxShadow: '0px 0px 3px lightgrey',
+              borderTop: '1px solid rgba(0,0,0,0.09)'
+            }}
+            value={this.state.value}
+          >
+            <BottomNavigationAction
+              label="Agenda"
+              value="agenda"
+              icon={<CalendarIcon />}
+            />
+            <BottomNavigationAction
+              label="Novo cliente"
+              value="addCustomer"
+              onClick={() => {
+                history.push('/admin/newCustomer');
+              }}
+              icon={<PersonAddIcon />}
+            />
+            <BottomNavigationAction
+              label="Retornos"
+              value="schedule"
+              onClick={() => {
+                history.push('/admin/nextAppointments');
+              }}
+              icon={<ScheduleIcon />}
+            />
+          </BottomNavigation>
+        </LoadingOverlay>
+      </div>
+    ));
+
+    return <LayoutWithRouter />;
+  }
+}
 
 const mapStateToProps = store => ({
   serviceProvider: store.serviceProvider
