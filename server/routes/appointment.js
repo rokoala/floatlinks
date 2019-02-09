cdcede46e4aa449097435d7935390e5a43ee1a0a
Marcel de Barros <mrdbarros@gmail.com>
2019-02-09 19:12:54 -0200

    appointment create adjusted

--- server/routes/appointment.js
+++ server/routes/appointment.js
@@ -12,83 +12,125 @@ Fawn.init(mongoose);
 //Create a new appointment
 router.post('/',  (req, res) => {
   validate(req.body, res, 'Request body is missing.');
+  slotId = ObjectId(req.body.slotId)
   customer = CustomerModel.aggregate([
     { $match: {_id: ObjectId(req.body.customerId)}},
     { $unwind: '$serviceProviders'},
     { $match: {'serviceProviders.providerId': ObjectId(req.body.serviceProviderId)}}]).exec()
-  serviceProvider = ServiceProviderModel.findById(req.body.serviceProviderId).exec()
-  slotId = ObjectId(req.body.slotId)
+
+  serviceProvider = ServiceProviderModel.aggregate([
+    { $match: {_id: ObjectId(req.body.serviceProviderId)}},
+    { $unwind: "$agenda.slots"},
+    { $match: {'agenda.slots._id': slotId}},
+    { $project: { password: 0 }}
+    ]).exec()
+    
+  let appointmentCreation = null
+  let slotReservation = null
+
   Promise.all([customer, serviceProvider])
     .then(  (results,slotId) => {
       customer = results[0][0]
-      serviceProvider = results[1]
-      if(!customer){
-        serviceProviderCreation = CustomerModel.findByIdAndUpdate(req.body.customerId, {
+      serviceProvider = results[1][0]
+      console.log(serviceProvider.agenda.slots)
+      if(!customer && serviceProvider && !serviceProvider.agenda.slots.isOccupied){
+        appointmentCreation = CustomerModel.findByIdAndUpdate(req.body.customerId, {
           $push: {
             serviceProviders: {
               providerId: serviceProvider._id,
               name: serviceProvider.name,
               providerPhone: serviceProvider.phone,
+              appointments: [{
+                appointmentSlotId: serviceProvider.agenda.slots._id,
+                appointmentDate: serviceProvider.agenda.slots.slotDate,
+                startTime: serviceProvider.agenda.slots.startTime,
+                appointmentDuration: serviceProvider.agenda.slots.slotDuration,
+                annotation: serviceProvider.agenda.slots.annotation
+              }]
             }
           }
         }).exec()
+
+        
       }
-      
-      appointmentCreation = CustomerModel.findOneAndUpdate({
-         _id: req.body.customerId 
-      },
-      {
-        $addToSet: {
-          'serviceProviders.$[outer].appointments': {
-            appointmentSlotId: slotId,
-            appointmentDate: '2001-08-05',
-            startTime: 1700,
-            appointmentDuration: 100,
-            annotation: 'teste'
+      else if(customer && serviceProvider && !serviceProvider.agenda.slots.isOccupied) {
+        appointmentCreation = CustomerModel.findOneAndUpdate({
+              _id: req.body.customerId 
+          },
+          {
+            $addToSet: {
+              'serviceProviders.$[outer].appointments': {
+                appointmentSlotId: serviceProvider.agenda.slots._id,
+                appointmentDate: serviceProvider.agenda.slots.slotDate,
+                startTime: serviceProvider.agenda.slots.startTime,
+                appointmentDuration: serviceProvider.agenda.slots.slotDuration,
+                annotation: serviceProvider.agenda.slots.annotation
+              }
+            }
+          },
+          {
+            "arrayFilters": [{ "outer.providerId": serviceProvider._id }] 
+          }).exec()
+      }
+      if(appointmentCreation){
+        slotReservation = ServiceProviderModel.findOneAndUpdate({
+          _id: req.body.serviceProviderId 
+          }, {
+          $set: {
+            'agenda.slots.$[outer].isOccupied': true
+          },
+          $push: {
+            'agenda.slots.$[outer].customer': {
+              customerId: customer._id,
+              name: customer.name,
+              phone: customer.phone
+            }
           }
+        },
+        {
+          "arrayFilters": [{ "outer._id": serviceProvider.agenda.slots._id }]
+          
         }
-      },
-      {
-        "arrayFilters": [{ "outer.providerId": serviceProvider._id }] 
-      }).exec() 
+        ).exec()
+      }
+       
       promiseResultHandler(res)(
-        appointmentCreation
+        slotReservation
       )
      
     
-      //console.log(results[0])
     }
   )
 });
 
 
-
-router.get('/', (req, res) => {
-  // res.send('test');
-  promiseResultHandler(res)(CustomerModel.find({}));
-});
-
-router.get('/:customerId', (req, res) => {
+router.get('/customer/:customerId', (req, res) => {
   promiseResultHandler(res)(
-    CustomerModel.find({ phone: req.params.customerPhone }).select('serviceProviders')
+    CustomerModel.find({ _id: req.params.customerId }).select('serviceProviders')
   );
 });
 
-router.get('/:customerId/:serviceProviderId', (req, res) => {
+router.get('/customer/:customerId/:serviceProviderId', (req, res) => {
   promiseResultHandler(res)(
-    CustomerModel.find({ _id: req.params.customerPhone, 'serviceProviders.providerId': req.params.serviceProviderId }, {'serviceProviders.$':1})
+    CustomerModel.find({ _id: req.params.customerId, 'serviceProviders.providerId': req.params.serviceProviderId }, {'serviceProviders.$':1})
   );
 });
 
-router.put('/:phone', (req, res) => {
+router.delete('/customer/:customerId/:serviceProviderId/:slotId', (req, res) => {
   promiseResultHandler(res)(
     CustomerModel.findOneAndUpdate(
       {
-        phone: req.params.phone
+        _id: req.params.customerId
       },
-      req.body,
       {
-        new: true
+        $pop: 'serviceProviders.$[outer].appointments.$[inner]'
+      },
+      {
+        "arrayFilters": [
+          { "outer.providerId": req.params.serviceProviderId },
+          { "inner.appointmentSlotId": req.params.slotId }
+      ]
+        
       }
     )
   );
