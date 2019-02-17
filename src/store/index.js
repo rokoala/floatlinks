4b6b0b17c66f1c09044f118f9d4d6d2b67b8b4fe
rokoga <rokoga@gmail.com>
2019-02-16 22:36:42 -0200

    redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Reducers } from '../reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const Store = createStore(Reducers, applyMiddleware(thunk));
+export const Store = createStore(
+  Reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
