8fb8198dab20ff6d106fbd45bdecf2a03124e214
rokoga <rokoga@gmail.com>
2019-02-16 22:36:22 -0200

    Add example of initialize models

---
+++ server/config/initialize.js
@@ -0,0 +1,45 @@
+const ServiceProviderModel = require('../models/serviceprovider.model');
+const CustomerModel = require('../models/customer.model');
+
+(async () => {
+  console.log('Cleaning all database...');
+  await ServiceProviderModel.remove({}).exec();
+  await CustomerModel.remove({}).exec();
+
+  const ServiceProviderExample = {
+    name: 'Dra. Alita',
+    phone: 1611112222,
+    password: 'alita',
+    defaultAppointmentDuration: 5,
+    agenda: {
+      slots: [
+        {
+          slotDate: new Date(),
+          startTime: 10,
+          slotDuration: 30,
+          isOccupied: false,
+          isPublic: true,
+          customer: null,
+          annotation: 'Horário teste'
+        }
+      ],
+      isOpen: true
+    }
+  };
+
+  const CustomerExample = {
+    name: 'John',
+    phone: 11111111111,
+    serviceProviders: []
+  };
+
+  console.log(`Creating Service Provider ${ServiceProviderExample.name}`);
+  console.log(ServiceProviderExample);
+  await ServiceProviderModel.create(ServiceProviderExample);
+
+  console.log(`Creating Customer ${CustomerExample.name}`);
+  console.log(CustomerExample);
+  await CustomerModel.create(CustomerExample);
+
+  process.exit();
+})();
